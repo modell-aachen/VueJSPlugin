c1cd4e0f18136781de0f540efd5ec299288e93be
Sven Meyer <meyer@modell-aachen.de>
2014-09-16 15:31:55 +0200

    Added JS utils
    Ticket: none

---
+++ pub/System/FlatSkinPlugin/src/js/qwiki/utils.js
@@ -0,0 +1,44 @@
+;(function ($, _, document, window, undefined) {
+  'use strict';
+
+  QWiki.plugins.NAME = {
+    name: 'NAME',
+
+    init: function( options ) {
+      if ( typeof options === 'object' ) {
+        $.extend( this, options );
+      }
+
+      this.bind();
+    },
+
+    bind: function() {
+      $('[data-split-string]').each( function() {
+        splitString( this, $(this).data('pattern') );
+      });
+    }
+  };
+
+  var splitString = function( elem, pattern ) {
+    if ( !pattern ) {
+      return;
+    }
+
+    var $self = $(elem);
+    var text = $self.data( 'origText' );
+
+    if ( !text ) {
+      text = $self.text();
+      $self.data( 'origText', text );
+    }
+
+    var regex = new RegExp( pattern );
+    if ( regex.test( text ) === false ) {
+      console.log('returning');
+      return;
+    }
+
+    var parts = text.split( regex );
+    $self.text( parts.join( ' ' ) );
+  };
+}(jQuery, window._, window.document, window));
