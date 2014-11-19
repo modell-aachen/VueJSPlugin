d7b19cb5f69e1e1927da8a6015f27c5f57fc7a8c
Sven Meyer <meyer@modell-aachen.de>
2014-11-19 17:06:53 +0100

    Added templates, styles and scripts to view attachments.
    Ticket: none

--- pub/System/FlatSkinPlugin/src/js/qwiki/attachments.js
+++ pub/System/FlatSkinPlugin/src/js/qwiki/attachments.js
@@ -14,13 +14,62 @@
 
     bind: function() {
       this.unbind();
+      $('.qw-attachments-container li').on( 'click', this, showDetails );
+      $('.qw-attachments-container').on( 'click.qw', this, closeDetails );
+      $('.qw-attachments-container .list .qw-back-btn').on( 'click', this, closeAttachments );
+      $('[data-attachtable-toggle]').on( 'click', this, showAttachments );
+      $('.qw-infobar').on( this.namespace + '.offcanvas.closing', this, closeAll );
     },
 
     unbind: function() {
-      // ToDo
+      $('.qw-attachments-container li').off( 'click', showDetails );
+      $('.qw-attachments-container').off( 'click.qw', closeDetails );
+      $('.qw-attachments-container .list .qw-back-btn').off( 'click', closeAttachments );
+      $('[data-attachtable-toggle]').off( 'click', showAttachments );
+      $('.qw-infobar').off( this.namespace + '.offcanvas.closing', closeAll );
     }
   };
 
-  var filter = function( self ) {};
-  var reset = function( self ) {};
+  var showAttachments = function( evt ) {
+    $('.qw-infobar > div > .content').addClass('offcanvas');
+    $('.qw-attachments-container').addClass('active');
+  };
+
+  var showDetails = function( evt ) {
+    var raw = $(this).find('.raw').text();
+    var json = $.parseJSON(raw);
+    if ( !json.comment ) {
+      json.comment = 'ToDo: i18n "no comment"';
+    }
+
+    var $cnt = $('.qw-attachments-container > .details');
+    $cnt.empty().qtemplate('add', $.extend({_type: 'qw.attachment.details'}, json));
+
+    $('.qw-attachments-container > .list').addClass('deactive');
+    $('.qw-attachments-container > .details').addClass('active');
+  };
+
+  var closeAttachments = function( evt ) {
+    $('.qw-infobar > div > .content').removeClass('offcanvas');
+    $('.qw-attachments-container').removeClass('active');
+  };
+
+  var closeDetails = function( evt ) {
+    if ( !$('.qw-attachments-container > .details').hasClass('active') ) {
+      return false;
+    }
+
+    var $src = $(evt.srcElement);
+    if ( $src.hasClass('qw-back-btn') ) {
+      $('.qw-attachments-container > .list').removeClass('deactive');
+      $('.qw-attachments-container > .details').removeClass('active');
+    }
+  };
+
+  var closeAll = function() {
+    $('.qw-attachments-container > .list').removeClass('deactive');
+    $('.qw-attachments-container > .details').removeClass('active');
+    $('.qw-infobar > div > .content').removeClass('offcanvas');
+    $('.qw-attachments-container').removeClass('active');
+  };
 }(jQuery, window._, window.document, window));
