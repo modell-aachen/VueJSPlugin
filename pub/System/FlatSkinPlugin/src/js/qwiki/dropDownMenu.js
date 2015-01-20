0104822557ae9a3a17c53b683ffea4b4c42b38b1
badakhshan <badakhshan@modell-aachen.de>
2015-01-20 15:37:40 +0100

    Implemented first version of qw-more-menu
    Ticket: none

--- pub/System/FlatSkinPlugin/src/js/qwiki/dropDownMenu.js
+++ pub/System/FlatSkinPlugin/src/js/qwiki/dropDownMenu.js
@@ -1,7 +1,7 @@
 
 jQuery(function($){ 
 	$('.qw-more-menu  > li').bind('mouseover', openSubMenu);
-		$('.qw-more-menu  > li').bind('mouseout', closeSubMenu);
+	$('.qw-more-menu  > li').bind('mouseout', closeSubMenu);
 		
 		function openSubMenu() {
 			$(this).find('ul').css('visibility', 'visible');	
