8f2b1e1272f0afcb401cb05ddbb539879a11e513
badakhshan <badakhshan@modell-aachen.de>
2015-01-20 13:18:40 +0100

    Restyled document head
    Ticket: none

---
+++ pub/System/FlatSkinPlugin/src/js/qwiki/dropDownMenu.js
@@ -0,0 +1,15 @@
+
+jQuery(function($){ 
+	$('.qw-more-menu  > li').bind('mouseover', openSubMenu);
+		$('.qw-more-menu  > li').bind('mouseout', closeSubMenu);
+		
+		function openSubMenu() {
+			$(this).find('ul').css('visibility', 'visible');	
+		}
+		
+		function closeSubMenu() {
+			$(this).find('ul').css('visibility', 'hidden');	
+		}
+
+	
+});
