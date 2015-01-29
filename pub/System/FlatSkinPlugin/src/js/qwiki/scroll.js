48568ecde1293ce5941a57b49c527c729d8a8c97
Sven Meyer <meyer@modell-aachen.de>
2015-01-29 13:40:17 +0100

    Enhanced plugin scroll
    Ticket: none

--- pub/System/FlatSkinPlugin/src/js/qwiki/scroll.js
+++ pub/System/FlatSkinPlugin/src/js/qwiki/scroll.js
@@ -13,6 +13,20 @@
         var data = $this.data('scrollcontainer');
         if ( data === 'parent' ) {
           $this.slimScroll({height: $this.parent().height()});
+        } else if ( /^\d+$/.test( data ) || /^\d+px$/i.test( data ) ) {
+          var height = data.replace( 'px', '' ) + 'px';
+          $this.slimScroll({height: height});
+        } else if ( data ) {
+          // data is selector
+          // if this is child of selector, adjust height
+          // else use selector height
+          var $data = $(data);
+          if ( $data.find( this ).length > 0 ) {
+            var h = $data.height() - $this.position().top - 10;
+            $this.slimScroll({height: h});
+          } else {
+            $this.slimScroll({height: $data.height()});
+          }
         } else {
           $this.slimScroll({height: 'auto'});
         }
