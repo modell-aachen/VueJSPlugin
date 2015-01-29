a8c2e3e804d072b87a155759e46b3c8d164803a5
Stephan Osthold <osthold@modell-aachen.de>
2015-01-29 11:52:04 +0100

    switchButton: Initialize checkmark correctly
    If the swtich is set to checked in the template or the user pressed F5
    the checkmark needs to be set accordingly.
    
    Ticket: #5754

--- pub/System/FlatSkinPlugin/src/js/qwiki/switchButton.js
+++ pub/System/FlatSkinPlugin/src/js/qwiki/switchButton.js
@@ -1,9 +1,14 @@
 jQuery(function($){ 
 
-	//if switch button has to be checked at the beggining,
-	// $(".switch input").on("load", function() {}
-
 	$(".switch input").on("change", function() {
 		$(this).parent().children("span").toggleClass("checkmark");
+	});
+
+	// initialize checkboxes
+	$(".switch input").each(function() {
+		var $this = $(this);
+		if($this.attr('checked')) {
+			$this.parent().children("span").addClass("checkmark");
+		}
 	});
 });
