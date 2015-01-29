f6b088c3dbccfbbc402dcf713c05d0c71bed416a
Sven Meyer <meyer@modell-aachen.de>
2015-01-29 11:56:39 +0100

    Updated SafeWiki signature for QWiki initialization
    Ticket: none

--- lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/FlatSkinPlugin.pm
+++ lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/FlatSkinPlugin.pm
@@ -1,8 +1,5 @@
 # This contains signatures for inline JS code contained in FlatSkinPlugin.
 [
-  # templates/foswiki.flat.tmpl
-  # foundation, pace & offline initialization
-  'vID6AqXTYyl0o/H4iaFwcpvgBnsS8YpnHoq0MK4Z9ws',
   # qwiki.js init
-  'ejEtq+hEqGTIFj/m1B558/uv7MMm5KZXO9U46IPmlJo',
+  '0zI/8ml2XaqLQBweoi37C4S9IYnNo6vB6E0SjiM1EJA',
 ];
