769b92efaa54ad48596a8bb6cc84dccc404de5a9
Jan Krüger <krueger@modell-aachen.de>
2015-02-24 14:50:18 +0100

    Quicksearch: port logic for expert search button (from old "show all")
    Ticket: none

--- pub/System/FlatSkinPlugin/src/js/qwiki/quicksearch.js
+++ pub/System/FlatSkinPlugin/src/js/qwiki/quicksearch.js
@@ -130,6 +130,11 @@
 
       $e.on( 'solrresults.quicksearch', [this, searcher], renderResults );
       $('[data-close-preview]').on( 'click.quicksearch', searcher, closePreview );
+
+      $('.qw-search-expert > a').on( 'click', function( evt ) {
+        location.assign(location.protocol +'//'+ location.host + foswiki.preferences.SCRIPTURLPATH +'/view'+ foswiki.preferences.SCRIPTSUFFIX +'/'+ foswiki.preferences.WEB +'/WebSearch');
+        return false;
+      });
     },
 
     unbind: function($e) {
@@ -175,7 +180,8 @@
         list.qtemplate('add', doc);
       }
 
-      $('.qw-total-results > a').on( 'click', function( evt ) {
+      $('.qw-search.expert > a').off( 'click' );
+      $('.qw-search-expert > a').on( 'click', function( evt ) {
         var term = data.responseHeader.params.q || '';
         location.assign(location.protocol +'//'+ location.host + foswiki.preferences.SCRIPTURLPATH +'/view'+ foswiki.preferences.SCRIPTSUFFIX +'/'+ foswiki.preferences.WEB +'/WebSearch#q='+ term.replace(/^\{.+?\}/, ''));
         return false;
