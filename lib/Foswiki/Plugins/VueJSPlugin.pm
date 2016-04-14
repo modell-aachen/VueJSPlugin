9cc94dfaaa934494038e06bbcf3a80bea5198d30
Daniel Supplieth <supplieth@modell-aachen.de>
2016-04-14 09:28:11 +0200

    Fix build.pm and improve config and add style.
    Ticket: none

--- lib/Foswiki/Plugins/VueJSPlugin.pm
+++ lib/Foswiki/Plugins/VueJSPlugin.pm
@@ -23,13 +23,23 @@ our $service;
 
 sub initPlugin {
     my ($topic, $web) = @_;
+    unless ( $Foswiki::cfg{Plugins}{JQueryPlugin}{Enabled} ) {
+        Foswiki::Func::writeWarning(
+            "VueJSPlugin is enabled but JQueryPlugin is not. Both must be installed and enabled for VUE."
+        );
+        return 0;
+    }
 
+    my $pluginURL = '%PUBURL%/%SYSTEMWEB%/VueJSPlugin';
+    my $dev = $Foswiki::cfg{Plugins}{VueJSPlugin}{UseSource} || 0;
+    my $suffix = $dev ? '' : '.min';
     Foswiki::Func::registerTagHandler( 'VUE', sub {
             my ( $session, $params, $topic, $web, $topicObject ) = @_;
-            my $app = $params->{_DEFAULT} || "Test";
-            Foswiki::Func::addToZone( 'script', 'VUEJS', '<script type="text/javascript" src="%PUBURL%/System/VueJSPlugin/vue.js"></script>', "JQUERYPLUGIN");
-            Foswiki::Func::addToZone( 'script', "VUEJS::$app", "<script type='text/javascript' src='%PUBURL%/System/VueJSPlugin/$app.js'></script>", "VUEJS");
-            return "<div id='$app'> {{ message }} </div>";
+            my $app = $params->{_DEFAULT} || "App";
+            Foswiki::Func::addToZone( 'script', 'VUEJSPLUGIN', "<script type='text/javascript' src='$pluginURL/vue$suffix.js'></script>", "JQUERYPLUGIN");
+            Foswiki::Func::addToZone( 'head', "VUEJS::STYLES", "<link rel='stylesheet' type='text/css' href='$pluginURL/vue.css'></link>");
+            Foswiki::Func::addToZone( 'script', "VUEJSPLUGIN::$app", "<script type='text/javascript' src='$pluginURL/$app.js'></script>", "VUEJSPLUGIN");
+            return "<div id='$app' v-cloak> {{ message }}</div>";
     });
 
     return 1;
--- lib/Foswiki/Plugins/VueJSPlugin/build.pl
+++ lib/Foswiki/Plugins/VueJSPlugin/build.pl
@@ -5,7 +5,7 @@ BEGIN {
 use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new Foswiki::Contrib::Build('VueJSContrib');
+$build = new Foswiki::Contrib::Build('VueJSPlugin');
 
 # Build the target on the command line, or the default target
 $build->build( $build->{target} );
