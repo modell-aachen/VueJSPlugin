2164d5955d8822097706beef7da8192565d5b295
Sven Meyer <meyer@modell-aachen.de>
2016-05-30 14:12:28 +0200

    First draft of new page layout
    Ticket: none

--- dev/tasks/config/concat.js
+++ dev/tasks/config/concat.js
@@ -7,7 +7,12 @@ module.exports = function(grunt) {
     fndt: {
       files: {
         'pub/System/FlatSkin/js/foundation.js': [
-          'node_modules/foundation-sites/js/foundation.core.js'
+          'node_modules/foundation-sites/js/foundation.core.js',
+          'node_modules/foundation-sites/js/foundation.util.mediaQuery.js',
+          'node_modules/foundation-sites/js/foundation.util.box.js',
+          'node_modules/foundation-sites/js/foundation.util.keyboard.js',
+          'node_modules/foundation-sites/js/foundation.util.nest.js',
+          'node_modules/foundation-sites/js/foundation.dropdownMenu.js'
         ]
       }
     },
