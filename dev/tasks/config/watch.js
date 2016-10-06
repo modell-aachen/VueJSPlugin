f74c1d253d8abcfbc584e5a848c1f92cd91691d8
Andre Tebart <tebart@modell-aachen.de>
2016-10-06 11:48:50 +0200

    Fixes styles.
    Ticket: none
    
    Release-Note: none

--- dev/tasks/config/watch.js
+++ dev/tasks/config/watch.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
     },
     sass: {
       files: ['dev/sass/**/*.scss'],
-      tasks: ['sass', 'cssmin']
+      tasks: ['sass', 'cssmin', 'css_wrap:compile']
     },
     js: {
       files: ['dev/js/**/*.js'],
