cebb31018858716bb226dc5240d0e2e6e89c8ae5
André Tebart <tebart@modell-aachen.de>
2018-04-11 13:30:39 +0200

    id:4755 Changes reporting of karma test results.

--- karma.conf.js
+++ karma.conf.js
@@ -44,7 +44,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec'],
+
+    specReporter: {
+        suppressPassed: true
+    },
 
 
     // web server port
