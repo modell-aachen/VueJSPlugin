e08fc753bb6a901840506db0a7722f26ea78977b
mmoscher <moscher@modell-aachen.de>
2018-05-09 11:28:32 +0200

    id: 5028 fixed default button color accordingly to component styleguide; fixed clean job

--- dev/tasks/config/clean.js
+++ dev/tasks/config/clean.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
   grunt.config.set('clean', {
     dev: [
-      'pub/System/FlatSkin/css/flatskin.*',
+      'pub/System/FlatSkin/css/flatskin*',
       'pub/System/FlatSkin/js/flatskin.*',
       'pub/System/FlatSkin/js/foundation.*',
       'pub/System/FlatSkin/fonts/opensans/**'
