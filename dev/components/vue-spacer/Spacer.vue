c86f193ef0cd6ff08a35862e85d3c162e3183fc0
Frederic Klein <klein@modell-aachen.de>
2018-05-16 17:52:00 +0200

    ID:3644 Add Spacer component
    id: 3644

---
+++ dev/components/vue-spacer/Spacer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div
+        :style="styleObject"
+        class="ma-spacer" />
+</template>
+
+<script>
+export default {
+    props: {
+        'factorVertical':{
+            type: String,
+            default: '1'
+        },
+        'factorHorizontal': {
+            type: String,
+            default: '1'
+        }
+    },
+    data() {
+        let factorVertical = this.factorVertical === 'auto' ? 'auto' : this.factorVertical*8+"px";
+        let factorHorizontal = this.factorHorizontal === 'auto' ? 'auto' : this.factorHorizontal*8+"px";
+
+        let styleObject = {
+            height: factorVertical,
+            width: factorHorizontal,
+        };
+        return {
+            styleObject
+        };
+    }
+};
+</script>
+
+<style lang="scss">
+</style>
