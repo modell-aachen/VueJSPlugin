f2cef685b130e11dc6647270e3e77c30db46a859
Daniel Supplieth <supplieth@modell-aachen.de>
2018-06-12 11:30:31 +0200

    id:5284 Unify test file naming.

--- karma.conf.js
+++ karma.conf.js
@@ -17,8 +17,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'tests/mockVisualMethods.js',
-      'tests/index.js'
+      'frontend-tests/mockVisualMethods.js',
+      'frontend-tests/Index.js'
     ],
 
 
@@ -30,9 +30,9 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'tests/index.js' : ['webpack', 'sourcemap'],
-      'tests/*_test.js': ['webpack', 'sourcemap'],
-      'tests/**/*_test.js': ['webpack', 'sourcemap']
+      'frontend-tests/Index.js' : ['webpack', 'sourcemap'],
+      'frontend-tests/*Spec.js': ['webpack', 'sourcemap'],
+      'frontend-tests/**/*Spec.js': ['webpack', 'sourcemap']
     },
 
     webpack: webpackConfig,
