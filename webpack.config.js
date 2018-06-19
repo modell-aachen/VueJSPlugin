6e00a95370511bbbd21efa37157df1241abd4ae9
mmoscher <moscher@modell-aachen.de>
2018-06-19 16:42:43 +0200

    id: 5084 added postcss loader

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,18 @@ module.exports = {
                 }
             },
             {
-              loader: 'postcss-loader'
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => {
+                  return [
+                    require('postcss-discard-duplicates'),
+                    require('postcss-discard-empty'),
+                    require('postcss-zindex'),
+                    require('autoprefixer')({browsers: ['last 2 version']})
+                  ];
+                },
+                sourceMap: true
+              }
             },
             {
                 loader: 'sass-loader',
