242acbcee4c6c922390fa0524ee5cb1e601fbb28
mmoscher <moscher@modell-aachen.de>
2018-06-20 15:11:03 +0200

    id: 5084 removed woff2 fonts

--- webpack.config.js
+++ webpack.config.js
@@ -79,7 +79,7 @@ module.exports = [{
           })
         },
         {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          test: /\.(woff|woff2|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
           use: [{
             loader: 'file-loader',
             options: {
