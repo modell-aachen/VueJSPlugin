6d10009492c4104930ff798ccf728707a11f4fa5
André Tebart <tebart@modell-aachen.de>
2018-09-18 10:47:14 +0200

    Adds VuexProxyPlugin.
    This plugin allows to register after mutation callbacks in the store.

--- dev/Frontend.js
+++ dev/Frontend.js
@@ -1,6 +1,7 @@
-import Vuex from 'vuex';
-import MAVueJsPlugin from './MAVueJsPlugin.js';
-import i18next from 'i18next';
+import Vuex from "vuex";
+import MAVueJsPlugin from "./MAVueJsPlugin.js";
+import i18next from "i18next";
+import VuexProxyPlugin from "./VuexProxyPlugin.js";
 
 class Frontend {
     constructor(options) {
@@ -9,10 +10,13 @@ class Frontend {
     setup() {
         this.options.vue.use(Vuex);
 
-        let store = new Vuex.Store();
+        let store = new Vuex.Store({
+            plugins: [VuexProxyPlugin._hook]
+        });
         const maVueJsPlugin = new MAVueJsPlugin(this.options);
         this.options.vue.use(maVueJsPlugin, {store});
 
+        this.options.vue.VuexProxyPlugin = VuexProxyPlugin;
         window.Vue = this.options.vue;
         window.i18next = i18next;
     }
